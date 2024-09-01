86fb34eeed435fc2881d32145ee194a92cb5c5c0
Mayank kaushik <mayankvip9@gmail.com>
2024-09-01 11:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const path= require('path');
+app.use(express.json());
+
 
 app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 const routes = require('./src/routes/routes.js');
